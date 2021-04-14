c5e9b599be65b4a3b35e9884e2485b44ff7f1462
Amit <amith@bsf.io>
2021-04-14 17:10:33 +0530

    svg loader added and updated build files

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,18 @@ module.exports = {
             {
                 test: /\.css$/,
                 loader: "style-loader!css-loader"
-            }
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                  {
+                    loader: 'svg-url-loader',
+                    options: {
+                      limit: 10000,
+                    },
+                  },
+                ],
+              },
         ]
     },
     externals: {
